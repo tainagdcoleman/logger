d61a0351453e10634de1588c474e6d2e8f181e16
Tainã Gariglio Dias <tainagarigliodias@Tainas-MacBook-Air.local>
2018-07-11 08:15:35 -0700

    Fixes overload issue

--- src/logger.h
+++ src/logger.h
@@ -22,7 +22,7 @@ public:
   static Logger getLogger();
   static void init(bool console, string file, string severity);
 
-  Logger(Logger const&) = delete;
-  void operator=(Logger const&) = delete;
+  //Logger(Logger const&) = delete;
+  //void operator=(Logger const&) = delete;
 
 };
